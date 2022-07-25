d51c319dac96caa98e0067b473ffdc5ff3c5a335
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-25 14:00:33 +0100

    created blank containers and components required

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+      <nav className='nav-pane'>
+
+      </nav>
+      <main className='beer-display'>
+
+      </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
