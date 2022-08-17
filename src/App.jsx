09d2ead19337fb31c2f664d86b4927197806f0fe
Thomastw374 <thomas.whiteley46@gmail.com>
2022-08-17 15:32:34 +0100

    Changed overflow css for cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import "./App.css";
 import "./sass/main.scss";
 
 function App() {
+  // States
+  // beers is an array of objects containing data on beers
   const [beers, setBeers] = useState([]);
+  // searchTerm is the search box input string
   const [searchTerm, setSearchTerm] = useState("");
+  // checked states are booleans representing checkbox state. False if unchecked, true if checked.
   const [highAbvChecked, setHighAbvChecked] = useState(false);
   const [classicRangeChecked, setClassicRangeChecked] = useState(false);
   const [ph4Checked, setPh4Checked] = useState(false);
 
+  // Cleans the search box input and then sets the state.
   const handleInput = (event) => {
     const cleanInput = event.target.value.toLowerCase();
     setSearchTerm(cleanInput);
   };
 
+  // Flip the checked states.
   const handleHighAbvCheck = (event) => {
     setHighAbvChecked(!highAbvChecked);
   };
@@ -28,12 +34,16 @@ function App() {
     setPh4Checked(!ph4Checked);
   };
 
+  // calls the getBeers function when filters are clicked
+
   useEffect(() => {
     getBeers(highAbvChecked, classicRangeChecked, ph4Checked);
   }, [highAbvChecked, classicRangeChecked, ph4Checked]);
 
   const getBeers = async (highAbvChecked, classicRangeChecked, ph4Checked) => {
     const beerData = [];
+
+    // loops through paginated data and appends each page of data to an array
 
     for (let pageNumber = 1; pageNumber < 6; pageNumber++) {
       const url =
@@ -44,8 +54,10 @@ function App() {
       beerData.push(await res.json());
     }
 
+    // flattens the beer array as pagination led to an array of arrays
     const flattenedBeerArray = await [].concat.apply([], beerData);
 
+    // filters based on a ph of less than 4
     if (ph4Checked === true) {
       const phFilteredBeerArray = flattenedBeerArray.filter((beer) => {
         return beer.ph < 4;
@@ -56,6 +68,7 @@ function App() {
     }
   };
 
+  // filters the beers based upon the value of the search term
   const filteredBeers = beers.filter((beer) => {
     const beerNameLower = beer.name.toLowerCase();
 
